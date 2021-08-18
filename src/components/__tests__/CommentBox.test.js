f046a3af93b4223887f24b6f3e9576ddf809d075
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-08-17 22:04:00 -0400

    initial commit with some basic component tests

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+it('has a text area and a button', () => {
+  const wrapped = mount(<CommentBox />);
+});
