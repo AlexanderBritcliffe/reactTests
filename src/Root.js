0fef0447547f24c7c4544ad7c4967e6b79ae57cd
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-08-18 19:24:19 -0400

    finished tests for comment list

--- src/Root.js
+++ src/Root.js
@@ -3,13 +3,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-export default (props) => {
+export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>
-      {props.children}
+    <Provider store={createStore(reducers, initialState)}>
+      {children}
     </Provider>
   );
 };
+
+//putting children and initial state on line 6 allows us to take
+//props away from children and initial state on like 8 and 9
 
 //line 9 is simply a refrence to <App /> in Root.js file
 //this gives access to a store by wrapping root around it
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
+
+//when we pull comments prpoperty from state object we are
+//specifically saying state.comments because back in reducer
+//index.js file we had assigned comments reducer to comments property
+
+//line 27 shows up as props to this component which is why we can refrence this.props.comments on line 6
+
+export default connect(mapStateToProps)(CommentList);
